902b560799bca2d41b92aadc62b2367f4906f7f4
Amrit Kaur <Amrit.Kaur@myob.com>
2017-11-23 10:15:28 +1300

    first command

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MYOB")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright © MYOB 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e520b599-6766-4a6c-83ea-eea217ca3ba3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
